00b88730e0010cdd27e954758afd7459906cbf60
Quentin McRee <imqtek@gmail.com>
2014-05-23 03:14:07 -0500

    fixed getting all question records.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,7 +4,7 @@ class HomeController extends BaseController {
 
 	public function showForm()
 	{
-        $questions = Question::with('options')->all();
+        $questions = Question::with('options')->get();
 		return View::make('form', array('questions' => $questions));
 	}
 
