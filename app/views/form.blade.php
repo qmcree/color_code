3be1631010f5dc993ac15c00a8a81c76402a07a3
Quentin McRee <imqtek@gmail.com>
2014-05-29 15:06:30 -0500

    added frontend packages. added layout and incorporated form view to use it.

--- app/views/form.blade.php
+++ app/views/form.blade.php
@@ -1,23 +1,37 @@
-<h2>Color Code Personality Test</h2>
+@extends('layout.master')
 
-<form method="post" accept-charset="UTF-8">
-    @foreach ($questions as $question)
-    <div class="form-group">
-        <h5>{{{ $question->text }}}</h5>
+@section('content')
+    <h2>Color Code Personality Test</h2>
 
-        <?php
-        $options = $question->options->toArray();
-        shuffle($options);
-        ?>
+    <form method="post" accept-charset="UTF-8">
+        @foreach ($questions as $question)
+        <div class="form-group">
+            <h5>{{{ $question->text }}}</h5>
 
-        @foreach ($options as $option)
+            <?php
+            $options = $question->options->toArray();
+            shuffle($options);
+            ?>
+
+            @foreach ($options as $option)
             <div class="radio">
                 <label>
-                    <input type="radio" name="options[{{ $question->id }}]" value="{{ $option['id'] }}">
+                    <input type="radio" name="options[{{ $question->id }}]" value="{{ $option['id'] }}" required>
                     {{{ $option['text'] }}}
                 </label>
             </div>
+            @endforeach
+        </div>
         @endforeach
-    </div>
-    @endforeach
-</form>
+        <div class="form-group">
+            <label for="full-name">Full name</label>
+            <input type="text" id="full-name" class="form-control" name="name" required />
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" id="email" class="form-control" name="email" required />
+        </div>
+        <button type="submit" class="btn btn-primary">Complete test</button>
+        <button type="reset" class="btn btn-default">Reset</button>
+    </form>
+@stop
---
+++ app/views/layout/master.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Color Code Personality Test</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="/packages/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="/packages/color_code/css/smp.css" />
+
+    <!-- <script src="//modernizr.com/downloads/modernizr-latest.js"></script> --> <!-- @todo Build production version. -->
+</head>
+<body>
+<!--[if lte IE 8]>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<div id="content" class="container">
+    @include('layout.partial.alert')
+
+    @yield('content')
+</div>
+
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> -->
+<script src="/packages/jquery/js/jquery.min.js"></script>
+<script src="/packages/bootstrap/js/bootstrap.min.js"></script>
+<script src="/packages/color_code/js/color_code.js"></script>
+</body>
+</html>
