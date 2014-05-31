453970bf3072eba1921267b4896395da863de580
mcreemq <mcreemq@ptk.org>
2014-05-31 18:15:00 -0500

    made response_id fillable

--- app/models/ResponseOption.php
+++ app/models/ResponseOption.php
@@ -5,7 +5,7 @@ namespace ColorCode;
 class ResponseOption extends \Eloquent
 {
     public $timestamps = false;
-    protected $fillable = array('option_id');
+    protected $fillable = array('response_id', 'option_id');
 
     public function option()
     {
