fc10112c10c07abffbaa4898e285e24c1df1e1cc
mcreemq <mcreemq@ptk.org>
2014-06-01 18:02:45 -0500

    include name in results view.

--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h3>Your color is <span class="color {{ strtolower($predominant) }}">{{ strtoupper($predominant) }}</span></h3>
 
+    <h4>Detailed results for <strong>{{{ $name }}}</strong></h4>
     <ol>
         @foreach ($tally as $category => $count)
         <li>{{ $category }} - {{ $count }}</li>
