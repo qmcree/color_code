2b9a0bf3815ee6ab55bdf9c7d5ea02ddb40e244d
mcreemq <mcreemq@ptk.org>
2014-06-02 00:59:15 -0500

    moved text around.

--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -1,9 +1,8 @@
 @extends('layout.master')
 
 @section('content')
-    <h3>Your color is <span class="color" style="color: {{ strtolower($predominant) }}">{{ strtoupper($predominant) }}</span></h3>
-
-    <h4>Detailed results for <strong>{{{ $name }}}</strong></h4>
+    <h3><strong>{{{ $name }}}</strong>, your predominant color is <span class="color" style="color: {{ strtolower($predominant) }}">
+            {{ strtoupper($predominant) }}</span></h3>
     <ul>
         @foreach ($tally as $category => $count)
         <li>{{ $category }} - {{ $count }}</li>
