f0ea6dfd58aa144e740d21a6338ccfd4e973c52d
mcreemq <mcreemq@ptk.org>
2014-06-02 17:09:56 -0500

    moved lib methods to new class in app/lib.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,75 +10,17 @@ class HomeController extends BaseController {
 
     public function process()
     {
-        $response = new ColorCode\Response();
-        $response->ip_address = Request::getClientIp();
-        $response->name = Input::get('name');
-        $response->email = Input::get('email');
-
-        DB::transaction(function() use ($response) {
-            $response->save();
-
-            foreach (Input::get('options') as $optionId) {
-                ColorCode\ResponseOption::create(array(
-                    'response_id' => $response->id,
-                    'option_id' => $optionId,
-                ));
-            }
-
-            DB::commit();
-        });
-
-        $tally = $this->getTally($response->id);
+        $response = new ColorCode\Lib\Response();
 
         // make predominant category first
-        arsort($tally);
+        $tally = arsort($response->tally);
 
         return View::make('results', array(
             'name' => Input::get('name'),
             'tally' => $tally,
-            'predominant' => $this->getPredominant($tally),
+            'predominant' => $response->tally,
         ));
     }
 
-    /**
-     * Gets tally of categories.
-     *
-     * @param integer $responseId
-     * @return array
-     */
-    protected function getTally($responseId)
-    {
-        $response = ColorCode\Response::with('responseOptions.option.category')->find($responseId);
 
-        $tally = array();
-        foreach ($response->responseOptions as $responseOption) {
-            $categoryName = $responseOption->option->category->name;
-
-            if (!array_key_exists($categoryName, $tally)) {
-                $tally[$categoryName] = 1;
-            } else {
-                $tally[$categoryName]++;
-            }
-        }
-
-        return $tally;
-    }
-
-    /**
-     * Determines predominant category based on tally.
-     *
-     * @param $tally
-     * @return string
-     */
-    protected function getPredominant($tally) {
-        $sortedCategories = array_keys($tally);
-        $values = array_values($tally);
-
-        // Predominant is WHITE if range of highest score and lowest score is <= 3.
-        if (($values[0] - end($values)) <= 3) {
-            return 'White';
-        } else {
-            return $sortedCategories[0];
-        }
-    }
 }
---
+++ app/lib/Response.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace ColorCode\Lib;
+
+use ColorCode;
+
+class Response
+{
+    public $responseId = 0;
+    public $tally = array();
+    public $predominant = '';
+
+    public function __construct()
+    {
+        $this->save();
+        $this->setTally();
+        $this->setPredominant();
+    }
+
+    /**
+     * Saves response and response options to database.
+     * Uses request parameters.
+     */
+    protected function save()
+    {
+        $response = new ColorCode\Response();
+        $response->ip_address = Request::getClientIp();
+        $response->name = Input::get('name');
+        $response->email = Input::get('email');
+
+        DB::transaction(function() use ($response) {
+            $response->save();
+
+            foreach (Input::get('options') as $optionId) {
+                ColorCode\ResponseOption::create(array(
+                    'response_id' => $response->id,
+                    'option_id' => $optionId,
+                ));
+            }
+
+            DB::commit();
+        });
+
+        $this->responseId = $response->id;
+    }
+
+    /**
+     * Gets tally of categories.
+     * @return array
+     */
+    protected function setTally()
+    {
+        $response = ColorCode\Response::with('responseOptions.option.category')->find($this->responseId);
+
+        $tally = array();
+        foreach ($response->responseOptions as $responseOption) {
+            $categoryName = $responseOption->option->category->name;
+
+            if (!array_key_exists($categoryName, $tally)) {
+                $tally[$categoryName] = 1;
+            } else {
+                $tally[$categoryName]++;
+            }
+        }
+
+        $this->tally = $tally;
+    }
+
+    /**
+     * Determines predominant category based on tally.
+     */
+    protected function setPredominant()
+    {
+        $sortedCategories = array_keys($this->tally);
+        $values = array_values($this->tally);
+
+        // Predominant is WHITE if range of highest score and lowest score is <= 3.
+        if (($values[0] - end($values)) <= 3) {
+            $this->predominant = 'White';
+        } else {
+            $this->predominant = $sortedCategories[0];
+        }
+    }
+} 
