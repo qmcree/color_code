be4dd76c6c4536e4d93e4bb631f5b9cdf54bd3f7
Quentin McRee <imqtek@gmail.com>
2014-06-18 23:32:28 -0500

    added script.js and included in layout.

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" type="text/css" href="/packages/color_code/css/color_code.css" />
 
     <!-- <script src="//modernizr.com/downloads/modernizr-latest.js"></script> --> <!-- @todo Build production version. -->
+    <script src="/packages/script_js/js/script.min.js" />
 </head>
 <body>
 <!--[if lte IE 8]>
